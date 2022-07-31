922b7d05bd15c530b57d8682f5844f00afd5555f
Daniel Assani <devasddaniel@gmail.com>
2022-07-31 05:27:20 +0200

    mise à jour des controleurs

--- app.py
+++ app.py
@@ -249,8 +249,8 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
- 
-  response = Artist.query.filter(Artist.name.ilike('%' + request.form['search_term'] + '%')).all()
+  response = {'data' : Artist.query.filter(Artist.name.ilike('%' + request.form['search_term'] + '%')).all(),
+  'count': Artist.query.filter(Artist.name.ilike('%' + request.form['search_term'] + '%')).count()}
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
